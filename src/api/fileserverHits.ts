512e212258283da89ec3a542a73440425f0d7f11
vextes <stevenfranger@yahoo.com>
2026-04-04 11:18:49 -0500

    moved fileserver hit api endpoints to /admin/ path

--- src/api/fileserverHits.ts
+++ src/api/fileserverHits.ts
@@ -2,7 +2,16 @@ import type { Request, Response } from "express";
 import { config } from "../config.js";
 
 export async function handlerFileserverHits(_:Request, res: Response) {
-    res.send(`Hits: ${config.fileserverHits}`);
+    res.set("Content-Type", "text/html; charset=utf-8");
+    res.send(`
+        <html>
+            <body>
+                <h1>Welcome, Chirpy Admin</h1>
+                <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+            </body>
+        </html>`
+        //Hits: ${config.fileserverHits}`
+    );
 }
 
 export async function handlerReset(_:Request, res: Response) {
--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ const PORT = 8080;
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerFileserverHits);
-app.get("/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerFileserverHits);
+app.get("/admin/reset", handlerReset);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/app`);
