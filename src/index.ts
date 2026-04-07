14dd64382045a143217978a18563a2ed72427c4d
vextes <stevenfranger@yahoo.com>
2026-04-07 14:39:29 -0500

    add length validation for posting Chirps

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerFileserverHits, handlerReset } from "./api/fileserverHits.js";
+import { handlerValidateChirps } from "./api/validation.js";
 
 const app = express();
 const PORT = 8080;
@@ -12,6 +13,8 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerFileserverHits);
 app.post("/admin/reset", handlerReset);
+
+app.post("/api/validate_chirp", handlerValidateChirps);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/app`);
