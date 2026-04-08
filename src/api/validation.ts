b59f386e78c4600b0077c4bbeb7751e24f67a4dd
vextes <stevenfranger@yahoo.com>
2026-04-08 10:06:35 -0500

    refactor to use express for json body parsing

--- src/api/validation.ts
+++ src/api/validation.ts
@@ -1,25 +1,15 @@
 import type { Request, Response } from "express";
 
 export async function handlerValidateChirps(req: Request, res: Response) {
-    let body = "";
+    type parameters = {
+        body: string;
+    };
+    const params: parameters = req.body;
 
-    //add data to body
-    req.on("data", (chunk) => {
-        body += chunk;
-    });
-
-    //parse body to json on end
-    req.on("end", () => {
-        try {
-            const parsedBody = JSON.parse(body);
-            if (parsedBody.body.length > 140) {
-                res.status(400).send({error: "Chirp is too long"})
-            }
-            else {
-                res.status(200).send({valid: true});
-            }
-        } catch (error) {
-            res.status(400).send({error: "Invalid JSON"});
-        }
-    });
+    if (params.body.length > 140) {
+        res.status(400).send({error: "Chirp is too long"})
+    }
+    else {
+        res.status(200).send({valid: true});
+    }
 }
